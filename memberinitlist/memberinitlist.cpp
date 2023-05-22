c6bf305a18a463efba1ecf70b785e8eda2ac299d
anggiaintan <anggia.intan.ft22@mail.umy.ac.id>
2023-05-22 20:37:07 +0700

    membuat access public

--- memberinitlist/memberinitlist.cpp
+++ memberinitlist/memberinitlist.cpp
@@ -7,3 +7,16 @@ private:
 	const int id;
 	string nama;
 	float nilai;
+
+public:
+	mahasiswa(int pId, string pNama, float pNilai) :id(pId), nama(pNama), nilai(pNilai) { //Memberi initialization list
+		//Definisi
+	}
+
+
+	~mahasiswa() {
+		cout << "Id	= " << id << endl;
+		cout << "Nama	= " << nama << endl;
+		cout << "Nilai	= " << nilai << endl;
+	}
+};
