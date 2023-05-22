7b609b32adb1dbef50de02041174ed10ab93774f
anggiaintan <anggia.intan.ft22@mail.umy.ac.id>
2023-05-22 20:17:51 +0700

    membuat access public

--- mainIA/ibu.h
+++ mainIA/ibu.h
@@ -1,1 +1,21 @@
 #pragma once
+#ifndef IBU_H
+#define IBU_H
+#include <iostream>;
+#include "anak.h"
+#include <vector>
+using namespace std;
+class ibu {
+public:
+	string nama;
+	vector<anak*> daftar_anak;
+
+	ibu(string pNama) :nama(pNama) {
+		cout << "Ibu \"" << nama << "\" ada\n";
+	}
+	~ibu() {
+		cout << "Ibu \"" << nama << "\" tidak ada\n";
+	}
+	void tambahAnak(anak*);
+	void cetakAnak();
+};
