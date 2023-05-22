f0325bd83ebacf4eb5f306b0cef3a49631ad84c4
anggiaintan <anggia.intan.ft22@mail.umy.ac.id>
2023-05-22 20:40:08 +0700

    membuat member

--- asosiasi/asosiasi.cpp
+++ asosiasi/asosiasi.cpp
@@ -5,3 +5,11 @@ using namespace std;
 class dokter;
 class pasien {
 public:
+	string nama;
+	vector<dokter*> daftar_dokter;
+	pasien(string pNama) :nama(pNama) {
+		cout << "Pasien \"" << nama << "\" ada\n";
+	}
+	~pasien() {
+		cout << "Pasien \"" << nama << "\" tidak ada\n";
+	}
